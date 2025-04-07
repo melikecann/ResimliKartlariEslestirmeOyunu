944b1008a1572dc2b078761d303ff15cd60cbed7
Melike Can <143530117+melikecann@users.noreply.github.com>
2025-04-07 10:28:11 +0300

    Delete

--- AndroidStudioProjects/HesapMakinesi/app/libs/MathParser.org-mXparser-v.6.1.0/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.mariuszgromada.math.mxparser"},{"l":"org.mariuszgromada.math.mxparser.mathcollection"},{"l":"org.mariuszgromada.math.mxparser.parsertokens"}];updateSearchResults();
