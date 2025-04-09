b6cd6dc682a62fdddb51429966340d1af08b61c3
Melike Can <melikeccan@gmail.com>
2025-04-09 12:24:04 +0300

    09.04

--- Desktop/MemoryCard/src/MatchCards.java
+++ Desktop/MemoryCard/src/MatchCards.java
@@ -4,67 +4,195 @@ import java.util.ArrayList;
 import javax.swing.*;
 
 public class MatchCards {
-    class Card{
+    class Card {
         String cardName;
         ImageIcon cardImageIcon;
-        
+
         Card(String cardName, ImageIcon cardImageIcon) {
             this.cardName = cardName;
             this.cardImageIcon = cardImageIcon;
-
         }
 
-        public String toString(){
+        public String toString() {
             return cardName;
         }
     }
 
-    String[] cardList = {   //track cardNames
-        "country cousin",
-        "donna duck",
-        "mickey mouse",
-        "mickeys amateur",
-        "moth and flame",
-        "postman",
-        "snow white",
-        "the giant",
-        "three little pigs",
-        "witch"
+    String[] cardList = {
+            "country cousin", "donna duck", "mickey mouse", "mickeys amateur", "moth and flame",
+            "postman", "snow white", "the giant", "three little pigs", "witch"
     };
 
-    int rows =4;
+    int rows = 4;
     int columns = 5;
     int cardWidth = 90;
     int cardHeight = 128;
 
-    ArrayList<Card> cardSet; // create a deck of cards with cardNames and cardImageIcons
+    ArrayList<Card> cardSet;
     ImageIcon cardBackImageIcon;
 
-    MatchCards(){
+    int boardWidth = columns * cardWidth;
+    int boardHeight = rows * cardHeight;
+
+    JFrame frame = new JFrame("Disney Match Cards");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
+    JPanel restartGamePanel = new JPanel();
+    JButton restartButton = new JButton();
+
+    int errorCount = 0;
+    ArrayList<JButton> board = new ArrayList<>();
+    Timer hideCardTimer;
+    boolean gameReady = false;
+    JButton card1Selected;
+    JButton card2Selected;
+
+    MatchCards() {
         setupCards();
         shuffleCards();
+
+        frame.setVisible(true);
+        frame.setLayout(new BorderLayout());
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        textLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Errors: " + errorCount);
+
+        textPanel.setPreferredSize(new Dimension(boardWidth, 30));
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
+
+        boardPanel.setLayout(new GridLayout(rows, columns));
+
+        for (int i = 0; i < cardSet.size(); i++) {
+            JButton tile = new JButton();
+            tile.setPreferredSize(new Dimension(cardWidth, cardHeight));
+            tile.setOpaque(true);
+            tile.setIcon(cardBackImageIcon);
+            tile.setFocusable(false);
+
+            int index = i;
+            tile.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (!gameReady) return;
+
+                    JButton clicked = (JButton) e.getSource();
+                    int idx = board.indexOf(clicked);
+                    if (clicked.getIcon().equals(cardBackImageIcon)) {
+                        clicked.setIcon(cardSet.get(idx).cardImageIcon);
+
+                        if (card1Selected == null) {
+                            card1Selected = clicked;
+                        } else if (card2Selected == null) {
+                            card2Selected = clicked;
+
+                            if (!card1Selected.getIcon().equals(card2Selected.getIcon())) {
+                                errorCount++;
+                                textLabel.setText("Errors: " + errorCount);
+                                hideCardTimer.start();
+                            } else {
+                                card1Selected = null;
+                                card2Selected = null;
+                            }
+                        }
+                    }
+                }
+            });
+
+            board.add(tile);
+            boardPanel.add(tile);
+        }
+
+        frame.add(boardPanel);
+
+        restartButton.setFont(new Font("Arial", Font.PLAIN, 16));
+        restartButton.setText("Restart Game");
+        restartButton.setPreferredSize(new Dimension(boardWidth, 30));
+        restartButton.setFocusable(false);
+        restartButton.setEnabled(false);
+        restartButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!gameReady) return;
+
+                gameReady = false;
+                restartButton.setEnabled(false);
+                card1Selected = null;
+                card2Selected = null;
+                shuffleCards();
+
+                for (int i = 0; i < board.size(); i++) {
+                    board.get(i).setIcon(cardBackImageIcon);
+                }
+
+                errorCount = 0;
+                textLabel.setText("Errors: " + errorCount);
+                hideCardTimer.start();
+            }
+        });
+
+        restartGamePanel.add(restartButton);
+        frame.add(restartGamePanel, BorderLayout.SOUTH);
+
+        frame.pack();
+        frame.setVisible(true);
+
+        hideCardTimer = new Timer(1500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                hideCards();
+            }
+        });
+
+        hideCardTimer.setRepeats(false);
+        hideCardTimer.start();
     }
 
-    void setupCards(){
-        cardSet = new ArrayList<Card>();
-        for  (String cardName : cardList)
-        {
-            // load each card img
-            Image cardImg = new ImageIcon(getClass().getResource("./img/" + cardName + ".jpg")). getImage();
-            ImageIcon carImageIcon = new ImageIcon(cardImg. getScaledInstance(cardWidth, cardHeight, java.awt.Image.SCALE_SMOOTH ));
-
-            // create card object and add to cardSet
+    void setupCards() {
+        cardSet = new ArrayList<>();
+        for (String cardName : cardList) {
+            Image cardImg = new ImageIcon(getClass().getResource("./img/" + cardName + ".jpg")).getImage();
+            ImageIcon carImageIcon = new ImageIcon(cardImg.getScaledInstance(cardWidth, cardHeight, Image.SCALE_SMOOTH));
             Card card = new Card(cardName, carImageIcon);
             cardSet.add(card);
         }
-        cardSet.addAll(cardSet); // listedeki kartlari double yapiyo 20 kart olcak 
+        cardSet.addAll(new ArrayList<>(cardSet)); // double cards = 20
 
-        //load the back card img
         Image cardBackImg = new ImageIcon(getClass().getResource("./img/back.jpg")).getImage();
-        cardBackImageIcon = new ImageIcon(cardBackImg.getScaledInstance(cardWidth, cardHeight, java.awt.Image.SCALE_SMOOTH));
+        cardBackImageIcon = new ImageIcon(cardBackImg.getScaledInstance(cardWidth, cardHeight, Image.SCALE_SMOOTH));
     }
 
-    void shuffleCards(){
-        System.out.println(cardSet);
+    void shuffleCards() {
+        for (int i = 0; i < cardSet.size(); i++) {
+            int j = (int) (Math.random() * cardSet.size());
+            Card temp = cardSet.get(i);
+            cardSet.set(i, cardSet.get(j));
+            cardSet.set(j, temp);
+        }
+    }
+
+    void hideCards() {
+        if (gameReady && card1Selected != null && card2Selected != null) {
+            card1Selected.setIcon(cardBackImageIcon);
+            card2Selected.setIcon(cardBackImageIcon);
+            card1Selected = null;
+            card2Selected = null;
+        } else {
+            for (JButton btn : board) {
+                btn.setIcon(cardBackImageIcon);
+            }
+            gameReady = true;
+            restartButton.setEnabled(true);
+        }
+    }
+
+    public static void main(String[] args) {
+        new MatchCards();
     }
 }
